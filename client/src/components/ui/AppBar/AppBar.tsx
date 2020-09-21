c72b2eb9d8417357b10de194b9444c37b97b2e38
janus022012 <janus022012@gmail.com>
2020-09-22 01:12:54 +0900

    メインメニュー作成用のディレクトリ構成を作成しました。

---
+++ client/src/components/ui/AppBar/AppBar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const AppBar = (props: any) => {
+  return (
+    <React.Fragment>
+      <h1>AppBar</h1>
+    </React.Fragment>
+  );
+};
+
+export default AppBar;
---
+++ client/src/components/ui/Tab/Tab.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Tab = (props: any) => {
+  return (
+    <React.Fragment>
+      <h1>Tab</h1>
+    </React.Fragment>
+  );
+};
+
+export default Tab;
