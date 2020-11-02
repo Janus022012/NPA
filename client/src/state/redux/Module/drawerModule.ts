1274d8b6153def324b9887a3d8e368904fa0568e
janus022012 <janus022012@gmail.com>
2020-11-02 18:55:31 +0900

    test for circleci1

--- client/src/state/redux/Module/drawerModule.ts
+++ client/src/state/redux/Module/drawerModule.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 import { DrawerState } from '../../../type/Module/DrawerState';
 
 const initialState: DrawerState = {
